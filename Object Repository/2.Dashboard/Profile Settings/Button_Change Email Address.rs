cb7f47828acfd306f7b04dc4772f817ed11db899
rr309548 <rr309548@CHLAPDABDULLA.as.ad.rrd.com>
2023-03-08 12:26:59 +0530

    Change Email Address

---
+++ Object Repository/2.Dashboard/Profile Settings/Button_Change Email Address.rs
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<MobileElementEntity>
+   <description></description>
+   <name>Button_Change Email Address</name>
+   <tag></tag>
+   <elementGuidId>fd9c26f3-81ef-48a1-9f67-9e2e2b1e0452</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <locator>//*[contains(@content-desc, 'Change Email Address')]</locator>
+   <locatorStrategy>XPATH</locatorStrategy>
+</MobileElementEntity>
---
+++ Object Repository/2.Dashboard/Profile Settings/TextBox_Enter Email Address.rs
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<MobileElementEntity>
+   <description></description>
+   <name>TextBox_Enter Email Address</name>
+   <tag></tag>
+   <elementGuidId>cb087dd0-d77d-4edf-a1b4-d282961421d0</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <locator>//*[@class = 'android.widget.EditText' and @resource-id = '' and (@text = '' or . = '')]</locator>
+   <locatorStrategy>XPATH</locatorStrategy>
+</MobileElementEntity>
